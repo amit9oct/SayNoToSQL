ae2af568523f05d59d4a58452d1aae9438aac774
Krishnan Govindraj <KrishnanGovindraj@outlook.com>
2017-07-20 12:24:38 +0530

    Added WitnessComparatorConstValue

--- SayNoToSQL/SNTSBackend/Semantics/WitnessFunctions.cs
+++ SayNoToSQL/SNTSBackend/Semantics/WitnessFunctions.cs
@@ -14,7 +14,8 @@ namespace SNTSBackend.Semantics
         public WitnessFunctions(Grammar grammar) : base(grammar) { }
 
         [WitnessFunction(nameof(Semantics.SelectWithoutWhere), 0)]
-        internal DisjunctiveExamplesSpec WitnessSelectWithoutWhere(GrammarRule rule, ExampleSpec spec) {
+        internal DisjunctiveExamplesSpec WitnessSelectWithoutWhere(GrammarRule rule, ExampleSpec spec)
+        {
 
             var ppExamples = new Dictionary<State, IEnumerable<object>>();
 
@@ -34,6 +35,82 @@ namespace SNTSBackend.Semantics
 
             return DisjunctiveExamplesSpec.From(ppExamples);
 
+        }
+
+        /* Inverse for :
+         *  Comparator(DataColumn column, DataTable tableList, string cmpSymbol, object constValue)
+         * */
+        [WitnessFunction(nameof(Semantics.Comparator), 3)]
+        internal DisjunctiveExamplesSpec WitnessComparatorConstValue(GrammarRule rule, ExampleSpec spec)
+        {
+            /* Inverse for the constValue field */
+            var ppExamples = new Dictionary<State, IEnumerable<object>>();
+
+            foreach (State input in spec.ProvidedInputs)
+            {
+                DataTable outputTable = (DataTable)spec.Examples[input];
+
+                var allPossibleSolutions = new List<object>();
+                string cmpSymbol = (string)input[rule.Body[2]]; // Get the comparison symbol
+                DataColumn column = (DataColumn)input[rule.Body[0]]; // Column
+                if (column.DataType == typeof(string))
+                {
+                    switch (cmpSymbol)
+                    {
+                        case "==":
+                            var valuesInColumn =
+                                outputTable.Rows.Cast<DataRow>().Select(t => t[column.ColumnName]).Distinct();
+                            // TODO: Get a linq guy to look this up
+                            if (valuesInColumn.Count() == 1)
+                            {
+                                allPossibleSolutions.Add(valuesInColumn.First());
+                            }
+                            // else, Keep it as empty
+
+                            break;
+                        default:
+                            // TODO: Unsupported datatype
+                            break;
+                    }
+
+                }
+                else if (column.DataType == typeof(double))
+                {
+
+                    switch (cmpSymbol)
+                    {
+                        case "==":
+                            var valuesInColumn =
+                                 outputTable.Rows.Cast<DataRow>().Select(t => t[column.ColumnName]).Distinct();
+                            // TODO: Get a linq guy to look this up
+                            if (valuesInColumn.Count() == 1)
+                            {
+                                allPossibleSolutions.Add(valuesInColumn.First());
+                            }
+                            // else, Keep it as empty
+                            break;
+
+                        case ">=":
+                            var minValueInColumn = outputTable.Rows.Cast<DataRow>().Select(t => t[column.ColumnName]).Min();
+                            allPossibleSolutions.Add((object)minValueInColumn);
+                            break;
+
+                        case "<=":
+                            var maxValueInColumn = outputTable.Rows.Cast<DataRow>().Select(t => t[column.ColumnName]).Max();
+                            allPossibleSolutions.Add((object)maxValueInColumn);
+                            break;
+
+                        default:
+                            // TODO: Unsupported datatype
+                            break;
+                    }
+                }
+
+                ppExamples[input] = allPossibleSolutions;
+
+            }
+
+            return DisjunctiveExamplesSpec.From(ppExamples);
         }
     }
 }
