c011634d8ec1ccbb8f94c7eec9e98f70ebffea00
Sharon Mathews <sharonsmathews@outlook.com>
2017-07-20 05:12:54 +0000

    Merged PR 1: Merge feature-csvWriter to master
    Code that we need later to write our data tables back to csv files

---
+++ SayNoToSQL/SNTSBackend/Parser/DatatableToCSVWriter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.IO;
+
+namespace SNTSBackend.Parser
+{
+    public static class DatatableToCSVWriter
+    {
+        public static void Write(DataTable table, String outputFilePath)
+        {
+            List<String> fields = new List<string>();
+            foreach(DataColumn col in table.Columns)
+            {
+                fields.Add(col.ColumnName);
+            }
+            using (StreamWriter sw = new StreamWriter(outputFilePath))
+            {
+                sw.WriteLine(String.Join(",", fields));
+            }
+            foreach (DataRow row in table.Rows)
+            {
+                fields = new List<string>();
+                foreach (DataColumn col in table.Columns)
+                {
+                    fields.Add(row[col].ToString());
+                }
+                using (StreamWriter sw = File.AppendText(outputFilePath))
+                {
+                    sw.WriteLine(String.Join(",", fields));
+                }
+            }
+        }
+    }
+}
--- SayNoToSQL/SayNoToSQL/Controllers/HomeController.cs
+++ SayNoToSQL/SayNoToSQL/Controllers/HomeController.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using ystem.Linq;
+using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
