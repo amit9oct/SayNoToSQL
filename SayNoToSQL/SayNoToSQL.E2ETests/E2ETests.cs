97a3f31340ce4e8589faaa795619c6b8de013a03
Amitayush Thakur <amitayusht@outlook.com>
2017-08-03 12:49:17 +0000

    Merged PR 20: Merge all E2E tests
    Added two E2E test

---
+++ SayNoToSQL/SayNoToSQL.E2ETests/E2ETests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+
+namespace SayNoToSQL.E2ETests
+{
+    [TestClass]
+    public class E2ETests
+    {
+        [TestMethod]
+        public void TestStart()
+        {
+            using (var web = new WebClient())
+            {
+                string response = web.DownloadString(@"http://localhost:25575/");
+                if(string.IsNullOrEmpty(response))
+                {
+                    throw new System.Exception("No Response Received");
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TestUpload()
+        {
+            using (var web = new WebClient())
+            {
+                string response = web.DownloadString(@"http://localhost:25575/FileUpload/");
+                if (string.IsNullOrEmpty(response))
+                {
+                    throw new System.Exception("No Response Received");
+                }
+            }
+        }
+    }
+}
